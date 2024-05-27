4ac0cfabbcee2005f27c5b3af960608d251d64ef
Akmal1796 <mhdakmal1796@gmail.com>
2024-05-27 08:06:46 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart) {
+    cart = 
+    [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let mathcingItem;
@@ -27,6 +36,8 @@ export function addToCart(productId) {
             quantity
         });
     }
+
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -39,4 +50,6 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
