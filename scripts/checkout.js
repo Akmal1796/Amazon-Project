8c1ba04dcdde8878ed65b0ae685c6b19f54593e2
Akmal1796 <mhdakmal1796@gmail.com>
2024-05-27 07:39:40 +0530

    Fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML = '';
 
@@ -33,7 +34,7 @@ cart.forEach((cartItem) => {
           ${mathcingProduct.name}
         </div>
         <div class="product-price">
-          $${mathcingProduct.priceCents / 100}
+          $${formatCurrency(mathcingProduct.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
@@ -55,7 +56,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${mathcingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -68,7 +69,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${mathcingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -81,7 +82,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${mathcingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
